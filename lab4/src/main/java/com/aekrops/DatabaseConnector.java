db9f1ec97ae4274bb2401c9aa9e47c525533b015
aekrops <svyatoslavtrostynski24@gmail.com>
2020-11-21 05:08:36 +0200

    added DatabaseConnector

---
+++ lab4/src/main/java/com/aekrops/DatabaseConnector.java
@@ -0,0 +1,30 @@
+package com.aekrops;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnector {
+    
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/trostynskyi_db?serverTimezone=Europe/Kiev";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = System.getenv("PASSWORD");
+    
+    private static Connection DATABASE_CONNECTION;
+    
+    public DatabaseConnector() {
+        
+    }
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            if (DATABASE_CONNECTION == null || DATABASE_CONNECTION.isClosed()) {
+                DATABASE_CONNECTION = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return DATABASE_CONNECTION;
+    }
+}
